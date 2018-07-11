65d45a07c0bffc31e3a8b33ca53c321af2ce4f2e
yuqiancha <ping_jw@126.com>
2018-07-11 16:16:25 +0800

    Create
    333

---
+++ connectsqlserver/connectsqlserver/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace connectsqlserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myQuery = "select * from Table_ZK";
+                dataGridView1.DataSource = SQLClass.ExcuteQueryUsingDataAdapter(myQuery);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("错误信息：" + ex.Message, "出现错误");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String CmdStr = "delete from Table_ZK where IP = '192.168.1.1'";
+            SQLClass.SqlExcuteCMD(CmdStr);
+
+            string myQuery = "select * from Table_ZK";
+            dataGridView1.DataSource = SQLClass.ExcuteQueryUsingDataAdapter(myQuery);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String CmdStr = "insert into Table_ZK(IP,来源,目的,时间,内容) values ('192.168.1.1','对接总线','地面设备','2017-07-01','5a5a5a5a5a5a5a5a')";
+            SQLClass.SqlExcuteCMD(CmdStr);
+
+            string myQuery = "select * from Table_ZK";
+            dataGridView1.DataSource = SQLClass.ExcuteQueryUsingDataAdapter(myQuery);
+        }
+
+
+    }
+}
---
+++ connectsqlserver/connectsqlserver/SQLClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connectsqlserver
+{
+    class SQLClass
+    {
+        public static string connsql = @"server=192.168.1.250;Database=TestDB1;uid=sa;pwd=Shanghai804";
+
+        public static void SqlExcuteCMD(String CmdStr)
+        {
+            using (var con = new SqlConnection(connsql))
+            using (var cmd = new SqlCommand(CmdStr, con))
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+        public static DataTable ExcuteQueryUsingDataReader(string myQuery)
+        {
+            DataTable dt = new DataTable();
+            using (var con = new SqlConnection(connsql))
+            using (var cmd = new SqlCommand(myQuery, con))
+            {
+                con.Open();
+                using (var dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    if (dr.HasRows)
+                    {
+                        dt.Load(dr);
+                    }
+                    //while(dr.Read())
+                    //{
+                    //    //....
+                    //}
+                }
+                return dt;
+            }
+        }
+
+        public static DataTable ExcuteQueryUsingDataAdapter(string myQuery)
+        {
+            using (SqlConnection con = new SqlConnection(connsql))
+            using (var dap = new SqlDataAdapter(myQuery, con))
+            {
+                // SqlDataAdapter dap = new SqlDataAdapter(myQuery, con);
+                DataTable dt = new DataTable();
+                dap.Fill(dt);
+                return dt;
+            }
+        }
+
+
+
+
+
+    }
+}
